9794f9db4dfba65403a5de80b035ce06b1160b99
Matheus <matheusbarkcaribe@gmail.com>
2025-09-06 11:28:51 -0300

    Base do Back End

---
+++ conectaBrenannd-api/conectaBrenannd-api/src/main/java/com/conecta/brenannd/ConectaBrenanndApiApplication.java
@@ -0,0 +1,13 @@
+package com.conecta.brenannd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaBrenanndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaBrenanndApiApplication.class, args);
+	}
+
+}
---
+++ conectaBrenannd-api/conectaBrenannd-api/src/test/java/com/conecta/brenannd/ConectaBrenanndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conecta.brenannd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaBrenanndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
